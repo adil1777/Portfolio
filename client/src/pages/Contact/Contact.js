9ab11ff185d9b0da66f4ec1249a402da5f8d406b
Mohd Adil <adilazmi760@gmail.com>
2023-12-30 19:21:20 +0530

    add fuctionality of icon( linkedin,github,fb)

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -1,35 +1,36 @@
 import React, { useState } from "react";
 import "./Contact.css";
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
 import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
-import contact from "../../assets/images/contact.png"
+import contact from "../../assets/images/contact.png";
 import { toast } from "react-toastify";
-import axios from 'axios';
-
+import axios from "axios";
 
 const Contact = () => {
-  const [name , setName] = useState("");
-  const [email , setEmail] = useState("");
-  const [msg , setMsg] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [msg, setMsg] = useState("");
 
   //handle Submit Button
-  const handleSubmit = async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
-      if(!name || !email || !msg){
+    try {
+      if (!name || !email || !msg) {
         toast.error("please Provide all fields");
         return;
       }
-      const res = await axios.post('http://localhost:8080/api/v1/portfolio/sendEmail' ,{name , email ,msg});
+      const res = await axios.post(
+        "http://localhost:8080/api/v1/portfolio/sendEmail",
+        { name, email, msg }
+      );
 
       //validation success
-      if(res.data.success){
+      if (res.data.success) {
         toast.success(res.data.message);
         setName("");
         setEmail("");
         setMsg("");
-      }
-      else{
+      } else {
         toast.error(res.data.message);
       }
     } catch (error) {
@@ -43,32 +44,45 @@ const Contact = () => {
       <div className=" contact" id="contact">
         <div className="card card0 border-0">
           <div className="row">
-          <Fade left>
-            <div className="col-md-6 col-lg-6 col-xl-6 col-sm-12">
-              <div className="card1">
-                <div className="row border-line">
-                  
-                    <img
-                      src={contact}
-                      alt="contact"
-                      className="image"
-                    />
-               
+            <Fade left>
+              <div className="col-md-6 col-lg-6 col-xl-6 col-sm-12">
+                <div className="card1">
+                  <div className="row border-line">
+                    <img src={contact} alt="contact" className="image" />
+                  </div>
                 </div>
               </div>
-            </div>
             </Fade>
             <Fade right>
-            <div className="col-lg-6 col-md-6">
-         
+              <div className="col-lg-6 col-md-6">
                 <div className="card2 d-flex card border-0 px-4 py-5">
                   <div className="row">
                     <div className="row">
                       <h6>
                         Contact With
-                        <BsLinkedin color="#0077b5" size={30} className="ms-2" />
-                        <BsGithub color="black" size={30} className="ms-2" />
-                        <BsFacebook color="#0077b5" size={30} className="ms-2" />
+                        <a
+                          href="https://www.linkedin.com/in/mohd-adil-305999215/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <BsLinkedin
+                            color="#0077b5"
+                            size={30}
+                            className="ms-2"
+                          />
+                        </a>
+                        <a
+                          href="https://github.com/adil1777?tab=repositories"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <BsGithub color="black" size={30} className="ms-2" />
+                        </a>
+                        <BsFacebook
+                          color="#0077b5"
+                          size={30}
+                          className="ms-2"
+                        />
                       </h6>
                     </div>
 
@@ -84,7 +98,7 @@ const Contact = () => {
                         placeholder="Enter your Name"
                         className="mb-3"
                         value={name}
-                        onChange= {(e)=> setName(e.target.value)}
+                        onChange={(e) => setName(e.target.value)}
                       />
                     </div>
                     <div className="row px-3">
@@ -94,7 +108,7 @@ const Contact = () => {
                         placeholder="Enter Your Email Address"
                         className="mb-3"
                         value={email}
-                        onChange= {(e)=> setEmail(e.target.value)}
+                        onChange={(e) => setEmail(e.target.value)}
                       />
                     </div>
                     <div className="row px-3">
@@ -104,18 +118,17 @@ const Contact = () => {
                         placeholder="Write your message"
                         className="mb-3"
                         value={msg}
-                        onChange= {(e)=> setMsg(e.target.value)}
-                       
+                        onChange={(e) => setMsg(e.target.value)}
                       />
                     </div>
                     <div className="row px-3">
-                      <button className="button"  onClick={handleSubmit}>
+                      <button className="button" onClick={handleSubmit}>
                         SEND MESSAGE
                       </button>
                     </div>
                   </div>
                 </div>
-            </div>
+              </div>
             </Fade>
           </div>
         </div>
