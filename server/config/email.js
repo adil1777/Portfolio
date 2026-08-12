3f9f7fd24570c59f9383455972300dcdcbbfe5cc
adil2024 <163632468+adil2024@users.noreply.github.com>
2026-08-12 19:55:46 +0530

    use gmail smtp host

--- server/config/email.js
+++ server/config/email.js
@@ -1,23 +1,22 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465
   auth: {
     user: process.env.SMTP_MAIL,
     pass: process.env.SMTP_PASS,
   },
 });
 
-transporter.verify((error) => {
-  if (error) {
-    console.error(
-      "[EMAIL] SMTP connection failed:",
-      error.message
-    );
-    return;
-  }
 
-  console.log("[EMAIL] SMTP server is ready");
-});
+transporter.verify()
+  .then(() => {
+    console.log("[EMAIL] SMTP server is ready");
+  })
+  .catch((err) => {
+    console.error("[EMAIL] SMTP verify failed:", err.message);
+  });
 
 module.exports = transporter;
